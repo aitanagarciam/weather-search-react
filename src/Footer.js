61006093cae16e3fe5c9f3b1e3ddd30b38f3a872
aitanagarciam <aitana.gm99@gmail.com>
2024-04-05 18:08:28 -0400

    Create Footer component

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+import react from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      This project was coded by
+      <a href="https://ve.linkedin.com/in/aitana-garcia" target="_blank">
+        Aitana Garcia
+      </a>
+      , is
+      <a
+        href="https://github.com/aitanagarciam/weather-search-engine"
+        target="_blank"
+      >
+        open-soured on GitHub
+      </a>
+      and hosted on
+      <a
+        href="https://weather-search-engine-aitana.netlify.app/"
+        target="_blank"
+      >
+        Netflify
+      </a>
+    </footer>
+  );
+}
